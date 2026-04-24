801cebb58029efa751702a0796cd53cf8d5a12c9
unknown <nathancao522@gmail.com>
2026-04-23 23:11:21 -0500

    implemented common average referencing

---
+++ src/preprocessing.py
@@ -0,0 +1,35 @@
+
+def car():
+    '''
+    Performs common average referencing
+    '''
+
+def bandpass():
+    '''
+    Performs bandpass filtering given lower and upper frequencies
+    '''
+
+def whitening():
+    '''
+    Performs whitening, time-wise, across channels
+    '''
+
+def thres_det():
+    '''
+    Performs threshold detection
+    '''
+
+def spike_sorting():
+    '''
+    Performs spike sorting
+    '''
+
+def binning():
+    '''
+    Performs binning
+    '''
+
+def smoothening():
+    '''
+    Performs smoothening
+    '''
